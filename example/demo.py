e74e2c463a86169792e15d615055d52f9a657074
Your Name <you@example.com>
2023-04-05 22:20:16 +0000

    update demo

--- example/demo.py
+++ example/demo.py
@@ -11,19 +11,19 @@ if __name__ == "__main__":
     part_cache_server = FeatureCacheServer(cpu_data)
     part_cache_server.cache_feature(cache_nids, False)
     assert part_cache_server.fetch_data(index).equal(
-        cpu_data[index.long()].cuda())
+        cpu_data[index.cpu().long()].cuda())
     del part_cache_server
 
     # full cache
     full_cache_server = FeatureCacheServer(cpu_data)
     full_cache_server.cache_feature(torch.tensor([]).int().cuda(), True)
     assert full_cache_server.fetch_data(index).equal(
-        cpu_data[index.long()].cuda())
+        cpu_data[index.cpu().long()].cuda())
     del full_cache_server
 
     # no cache
     no_cache_server = FeatureCacheServer(cpu_data)
     no_cache_server.cache_feature(torch.tensor([]).int().cuda(), False)
     assert no_cache_server.fetch_data(index).equal(
-        cpu_data[index.long()].cuda())
+        cpu_data[index.cpu().long()].cuda())
     del no_cache_server
